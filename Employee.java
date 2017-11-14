6e15a8ee2fae23bb2c38a6616f7423818f82b6dd
hemala <hemaladevanan125@gmail.com>
2017-11-14 09:30:43 +0530

    Hibernate class 1

--- Employee.java
+++ Employee.java
@@ -1,14 +1,26 @@
 package org.emp;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
-	
+	@Id
 	private int id;
 	private String name;
 	private String email;
 	private long phone;
-	private boolean gender;
 	public int getId() {
 		return id;
+	}
+	public Employee() {
+		
+	}
+	public Employee(int id, String name, String email, long phone) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
 	}
 	public void setId(int id) {
 		this.id = id;
@@ -30,12 +42,6 @@ public class Employee {
 	}
 	public void setPhone(long phone) {
 		this.phone = phone;
-	}
-	public boolean isGender() {
-		return gender;
-	}
-	public void setGender(boolean gender) {
-		this.gender = gender;
 	}
 	
 }
--- Solution.java
+++ Solution.java
@@ -4,49 +4,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.emp.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
-		Employee emp = new Employee();
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+        System.out.print("Enter Id : ");
+		int id = Integer.valueOf(bf.readLine());
+		System.out.print("Enter Name : ");
+		String name = bf.readLine();
+		System.out.print("Enter Email : ");
+		String email = bf.readLine();
+		System.out.print("Enter Phone : ");
+		long phone = Long.valueOf(bf.readLine());
+		
+		Employee   emp = new Employee(id, name, email, phone);
 		
 		System.out.println(emp.getId());
 		System.out.println(emp.getName());
 		System.out.println(emp.getEmail());
 		System.out.println(emp.getPhone());
-		System.out.println(emp.isGender());
 		
-		emp.setId(5);
-		emp.setName("Bhuvi");
-		emp.setEmail("bhuvi@gmail.com");
-		emp.setPhone(9876543212l);
-		emp.setGender(true);
-
-		System.out.println(emp.getId());
-		System.out.println(emp.getName());
-		System.out.println(emp.getEmail());
-		System.out.println(emp.getPhone());
-		System.out.println(emp.isGender());
-		
-		Employee emp1 = new Employee();
-		
-		System.out.println(emp1.getId());
-		System.out.println(emp1.getName());
-		System.out.println(emp1.getEmail());
-		System.out.println(emp1.getPhone());
-		System.out.println(emp1.isGender());
-		
-		emp1.setId(7);
-		emp1.setName("Hema");
-		emp1.setEmail("hema@gmail.com");
-		emp1.setPhone(9876556712l);
-		emp1.setGender(true);
-
-		System.out.println(emp1.getId());
-		System.out.println(emp1.getName());
-		System.out.println(emp1.getEmail());
-		System.out.println(emp1.getPhone());
-		System.out.println(emp1.isGender());
+		session.close();
 	}
 
 }
